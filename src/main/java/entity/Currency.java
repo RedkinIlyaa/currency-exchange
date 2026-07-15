ad3d5ef326e5779510ff5e3e44c5a51de047aebe
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-15 18:09:42 +0300

    feat(java.entity): add Currency

---
+++ src/main/java/entity/Currency.java
@@ -0,0 +1,15 @@
+package entity;
+
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Currency {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+}
