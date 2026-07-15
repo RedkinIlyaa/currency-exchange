3d166e9b28cfa0611e7d480e3ac08fd2ff0a20c4
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-15 17:46:06 +0300

    chore: add DataSourceManager & ConnectionException

---
+++ src/main/java/util/DataSourceManager.java
@@ -0,0 +1,35 @@
+package util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import exception.ConnectionException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DataSourceManager {
+
+    private static final HikariConfig hikariConfig = new HikariConfig();
+    private static final HikariDataSource hihikariDataSource;
+
+    static {
+        hikariConfig.setJdbcUrl(PropertiesUtil.getUrl());
+        hikariConfig.setUsername(PropertiesUtil.getUser());
+        hikariConfig.setPassword(PropertiesUtil.getPassword());
+        hikariConfig.setMaximumPoolSize(8);
+        hikariConfig.setMinimumIdle(2);
+        hihikariDataSource = new HikariDataSource(hikariConfig);
+    }
+
+    public static Connection getConnection() {
+        try {
+            return hihikariDataSource.getConnection();
+        } catch (SQLException e) {
+            throw new ConnectionException("Failed to obtain Connection from HikariCP pool", e);
+        }
+    }
+
+}
