cd98bbe99c5fa682157e74a46f5d44472dde59ec
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-17 22:16:36 +0300

    feat(service): create CurrencyService

---
+++ src/main/java/service/CurrencyService.java
@@ -0,0 +1,30 @@
+package service;
+
+import dao.CurrencyDao;
+import dto.CurrencyDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CurrencyService {
+
+    private static final CurrencyService currencyService = new CurrencyService();
+    private final CurrencyDao currencyDao = CurrencyDao.getCurrencyDao();
+
+    public List<CurrencyDto> getAllCurrencies() {
+        return currencyDao.getCurrenciesList().stream().map(
+                currency -> CurrencyDto.builder()
+                        .id(currency.getId())
+                        .name(currency.getFullName())
+                        .code(currency.getCode())
+                        .sign(currency.getSign())
+                        .build()
+        ).toList();
+    }
+
+    public static CurrencyService getInstance() {
+        return currencyService;
+    }
+}
