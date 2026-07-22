f0c950d6c338a96d6256d494a8e4dc2c5cc814e4
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-22 11:59:30 +0300

    chore(ExceptionHandlingFilter): add Logger and log & setCharacterEncoding

--- src/main/java/filter/ExceptionHandlingFilter.java
+++ src/main/java/filter/ExceptionHandlingFilter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,6 +14,7 @@ import java.util.Map;
 @WebFilter(value = "/currencies")
 public class ExceptionHandlingFilter implements Filter {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlingFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -26,11 +29,13 @@ public class ExceptionHandlingFilter implements Filter {
                 throw runtimeException;
 
             httpServletResponse.reset();
+            httpServletResponse.setCharacterEncoding("UTF-8");
             httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             httpServletResponse.setContentType("application/json");
             Map<String, String> map = new HashMap<>();
             map.put("message", "Internal server error");
             objectMapper.writeValue(httpServletResponse.getOutputStream(), map);
+            logger.error("Unhandled exception during request processing", runtimeException);
         }
     }
 }
