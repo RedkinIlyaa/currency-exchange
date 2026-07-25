7ab928b59fe72b65bd82f4131846df3e69ba3323
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-25 11:09:08 +0300

    feat(CurrencyService): add getCurrencyByCode()

--- src/main/java/service/CurrencyService.java
+++ src/main/java/service/CurrencyService.java
@@ -6,6 +6,8 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CurrencyService {
@@ -22,6 +24,16 @@ public class CurrencyService {
                         .sign(currency.getSign())
                         .build()
         ).toList();
+    }
+
+    public Optional<CurrencyDto> getCurrencyByCode(String code) {
+        return currencyDao.findByCode(code.toUpperCase(Locale.ENGLISH))
+                .map(currency -> CurrencyDto.builder()
+                        .id(currency.getId())
+                        .name(currency.getFullName())
+                        .code(currency.getCode())
+                        .sign(currency.getSign())
+                        .build());
     }
 
     public static CurrencyService getInstance() {
