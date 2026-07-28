755371faf9e4ff7c3a33fa3e4c00e7092750c4d7
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-07-28 14:32:00 +0300

    add parent class - NotFoundException
    create new ExchangeRateNotFoundException extends NotFoundException
    update CurrencyNotFoundException -> (extends NotFoundException)

---
+++ src/main/java/exception/ExchangeRateNotFoundException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class ExchangeRateNotFoundException extends NotFoundException {
+    public ExchangeRateNotFoundException(String message) {
+        super(message);
+    }
+}
