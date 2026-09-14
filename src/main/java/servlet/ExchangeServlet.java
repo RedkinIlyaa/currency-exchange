1349e6e283f30a6e831650f6f47d4d42a8867671
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-09-14 22:37:00 +0300

    add check for parameters in ExchangeServlet

--- src/main/java/servlet/ExchangeServlet.java
+++ src/main/java/servlet/ExchangeServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.ExchangeRateDto;
+import exception.invalid.InvalidException;
 import exception.invalid.InvalidExclusionOfRequiredParameter;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import service.ExchangeRateService;
 
 import java.io.IOException;
+import java.util.Map;
 
 
 @WebServlet(value = "/exchange")
@@ -19,6 +21,14 @@ public class ExchangeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, String[]> parameterMap = checkRequestParameters(req);
+
+        for (Map.Entry<String, String[]> entry: parameterMap.entrySet()) {
+            String[] value = entry.getValue();
+            if (value.length != 1)
+                throw new InvalidException("Parameter " + entry.getKey() + " has more than one value");
+        }
+
         String fromCurrency = req.getParameter("from");
         String toCurrency = req.getParameter("to");
         String amount = req.getParameter("amount");
@@ -39,5 +49,23 @@ public class ExchangeServlet extends HttpServlet {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Map<String, String[]> checkRequestParameters(HttpServletRequest req) {
+        Map<String, String[]> parameterMap;
+        try {
+            parameterMap = req.getParameterMap();
+        } catch (IllegalStateException e) {
+            throw new InvalidException("Request parameters are malformed or incorrectly encoded.");
+        }
+        if (parameterMap.size() != 3)
+            throw new InvalidException("There must be exactly 3 parameters in GET /exchange: from, to, amount");
+        if (!parameterMap.containsKey("from"))
+            throw new InvalidException("Missing from parameter in the request");
+        if (!parameterMap.containsKey("to"))
+            throw new InvalidException("Missing to parameter in the request");
+        if (!parameterMap.containsKey("amount"))
+            throw new InvalidException("Missing amount parameter in the request");
+        return parameterMap;
     }
 }
