77017309b25466085a2dd68134aca8e56520905e
Ilya Redkin <RedkinIlyaa@yandex.ru>
2026-09-19 17:58:06 +0300

    fix: update insert_exchange_rates

--- sql/insert_exchange_rates.sql
+++ sql/insert_exchange_rates.sql
@@ -1,20 +1,219 @@
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (3, 4, 94.000000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (2, 3, 0.850000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES ( 2, 7, 7.210000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (5, 2, 1.340000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (2, 6, 147.500000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (27, 28, 52.500000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (2, 27, 650.000000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (7, 4, 12.100000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (9, 8, 62.000000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (7, 8, 12.000000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (1, 11, 0.500000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (2, 11, 48.390000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (3, 11, 56.300000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (4, 11, 0.560000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (5, 11, 65.460000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (6, 11, 0.560000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (10, 12, 20.000000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (7, 11, 7.220000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (8, 11, 59.890000);
-INSERT INTO public.exchange_rates (base_currency_id, target_currency_id, rate) VALUES (2, 4, 95.000000);
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'EUR'),
+           (SELECT id FROM public.currencies WHERE code = 'RUB'),
+           94.000000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'EUR'),
+           0.850000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'CNY'),
+           7.210000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'GBP'),
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           1.340000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'JPY'),
+           147.500000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'MXN'),
+           (SELECT id FROM public.currencies WHERE code = 'SGD'),
+           52.500000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'MXN'),
+           650.000000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'CNY'),
+           (SELECT id FROM public.currencies WHERE code = 'RUB'),
+           12.100000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'CAD'),
+           (SELECT id FROM public.currencies WHERE code = 'CHF'),
+           62.000000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'CNY'),
+           (SELECT id FROM public.currencies WHERE code = 'CHF'),
+           12.000000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'AUD'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           0.500000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           48.390000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'EUR'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           56.300000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'RUB'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           0.560000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'GBP'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           65.460000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'JPY'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           0.560000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'NZD'),
+           (SELECT id FROM public.currencies WHERE code = 'THB'),
+           20.000000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'CNY'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           7.220000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'CHF'),
+           (SELECT id FROM public.currencies WHERE code = 'TRY'),
+           59.890000
+       );
+
+INSERT INTO public.exchange_rates (
+    base_currency_id,
+    target_currency_id,
+    rate
+)
+VALUES (
+           (SELECT id FROM public.currencies WHERE code = 'USD'),
+           (SELECT id FROM public.currencies WHERE code = 'RUB'),
+           95.000000
+       );
